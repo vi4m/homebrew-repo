f1ed682753e9924aa0eb570559852fd214496419
✪ vi4m Marcin Kliks <vi4m@users.noreply.github.com>
2017-01-05 16:16:29 +0100

    Update vax.rb

--- vax.rb
+++ vax.rb
@@ -1,9 +1,9 @@
 class Vax < Formula
   desc "Tool for vagrant-style development workflow on pure Docker"
   homepage ""
-  url "https://github.com/vi4m/vax/archive/0.1.2.tar.gz"
-  version "0.1.2"
-  sha256 "8f27ab7f5b7ae238f8e86a3c7db7b1f53fb3bf500d0dcd6c52235839e8af3235"
+  url "https://github.com/vi4m/vax/archive/0.1.3.tar.gz"
+  version "0.1.3"
+  sha256 "7b8080a0fc10cc0c27c9b833d150effe08826604f219f729d1e2a38d63595566"
 
   def install
     system "unset CC; swift build -c release"
